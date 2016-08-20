83b79eb55432b88c8020f42aa133f6993a678772
Student <Student@Mac-mini-7.local>
2016-08-20 12:03:15 -0600

    lunch

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 var people = function () {
-
     var victs = function () {
-        if (/^\+?\d+$/.test(str)){
+        if (/^\+?\d+$/.test(str)) = true {
             var userAnswer = prompt("Number of victims?","# of victims.");
                 console.log(userAnswer);
                 informaton ()
                 return userAnswer;
-                }
+             }
             else {
                prompy("Not at number");
                 victs();
+            }
         } 
-    }
+    
 
-    var vols = function () {
-        var vols = function () {
-            if (/^\+?\d+$/.test(str)){
-                var userAnswer = prompt("Number of victims?","# of victims.");
+     var vols = function () {
+        if (/^\+?\d+$/.test(str)) = true{
+            var userAnswer = prompt("Number of victims?","# of victims.");
                 console.log(userAnswer);
                 informaton ()
-                return userAnswer;
+            return userAnswer;
             }
             else {
                prompy("Not at number");
                 vols();
             }
         }
+    
+}
 

fae040aa77bac8c7222fe6614c11c49213adda79
Student <Student@Mac-mini-7.local>
2016-08-20 10:48:08 -0600

    midday

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-var victs = function () {
-       var userAnswer = prompt("Number of victims?");
-    console.log(userAnswer);
-    return userAnswer;
-}
+var people = function () {
+
+    var victs = function () {
+        if (/^\+?\d+$/.test(str)){
+            var userAnswer = prompt("Number of victims?","# of victims.");
+                console.log(userAnswer);
+                informaton ()
+                return userAnswer;
+                }
+            else {
+               prompy("Not at number");
+                victs();
+        } 
+    }
+
+    var vols = function () {
+        var vols = function () {
+            if (/^\+?\d+$/.test(str)){
+                var userAnswer = prompt("Number of victims?","# of victims.");
+                console.log(userAnswer);
+                informaton ()
+                return userAnswer;
+            }
+            else {
+               prompy("Not at number");
+                vols();
+            }
+        }
 
